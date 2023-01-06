db3eaaa10c9d44db358bca42535ea2ad7848942c
DreasWeiss <dreasweiss@gmail.com>
2023-01-06 23:05:00 +0300

    handelbars(express) added

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 const app = express();
 
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'views','index.html'));
+    res.render('index');
 });
 
 
