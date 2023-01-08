96da7dffbe36a437c523c26a039383d091de9b1f
DreasWeiss <dreasweiss@gmail.com>
2023-01-08 18:26:11 +0300

    l29 dynamic parameters=>  added new method to model Course: .fingById(id) + added new route in courses routes + added new layout empty + added course.hbs and some css for it

--- models/course.js
+++ models/course.js
@@ -54,6 +54,11 @@ class Course{
             );
         });
     }
+
+    static async getById(id){
+        const courses = await Course.getAll();
+        return courses.find(course => course.id === id);
+    }
 }
 
 module.exports = Course;
