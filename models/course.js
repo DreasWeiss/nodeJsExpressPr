d8df082de978b991755f359214b9e1321ab380db
DreasWeiss <dreasweiss@gmail.com>
2023-01-08 19:27:44 +0300

    l30 editing course feature => added new method for Course class .update() + added course-edit page + added 2 routes to courses routes

--- models/course.js
+++ models/course.js
@@ -59,6 +59,27 @@ class Course{
         const courses = await Course.getAll();
         return courses.find(course => course.id === id);
     }
+
+    static async update(course){
+        const courses = await Course.getAll();
+
+        const idx = courses.findIndex(c => c.id === course.id);
+        courses[idx] = course;
+
+        return new Promise ((resolve, reject) => {
+            fs.writeFile(
+                path.join(__dirname,'..','data','courses.json'),
+                JSON.stringify(courses),
+                (err) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                }
+            );
+        }); 
+    }
 }
 
 module.exports = Course;
--- routes/courses.js
+++ routes/courses.js
@@ -20,4 +20,22 @@ router.get('/:id', async (req, res) => {
     });
 });
 
+router.get('/:id/edit', async (req, res) => {
+    if (!req.query.allow) {
+        return res.redirect('/');
+    }
+
+    const course = await Course.getById(req.params.id);
+
+    res.render('course-edit', {
+        title: `Edit ${course.title}`,
+        course
+    });
+});
+
+router.post('/edit', async (req, res) => {
+    await Course.update(req.body);
+    res.redirect('/courses');
+});
+
 module.exports = router;
