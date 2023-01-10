87e5db13ddea8b0c74787e50b25498b08f0fd6de
DreasWeiss <dreasweiss@gmail.com>
2023-01-11 01:09:03 +0300

    l32 Cart model: - created new json file for cart - added methods add and fetch for model Cart - routes cart has some changes => for get added isCart, courses and price

--- routes/cart.js
+++ routes/cart.js
@@ -1,46 +1,23 @@
-// const {Router} = require('express');
-// const Course = require('../models/course');
-// const Cart = require('../models/cart');
-// const router = Router();
-
-// router.post('/add', async (req, res) => {
-//     const course = await Course.getById(req.body.id);
-//     await Cart.add(course);
-//     res.redirect('/cart');
-// });
-
-// router.get('/', async (req, res) => {
-//     const cart = await Cart.fetch();
-//     res.render('cart', {
-//         title: 'Cart',
-//         isCart: true,
-//         courses: cart.courses,
-//         price: cart.price
-//     });
-// });
-
-
-// module.exports = router;
-
-const {Router} = require('express')
-const Cart = require('../models/cart')
-const Course = require('../models/course')
-const router = Router()
+const {Router} = require('express');
+const Course = require('../models/course');
+const Cart = require('../models/cart');
+const router = Router();
 
 router.post('/add', async (req, res) => {
-  const course = await Course.getById(req.body.id)
-  await Cart.add(course)
-  res.redirect('/cart')
-})
+    const course = await Course.getById(req.body.id);
+    await Cart.add(course);
+    res.redirect('/cart');
+});
 
 router.get('/', async (req, res) => {
-  const cart = await Cart.fetch()
-  res.render('cart', {
-    title: 'Корзина',
-    isCard: true,
-    courses: cart.courses,
-    price: cart.price
-  })
-})
+    const cart = await Cart.fetch();
+    res.render('cart', {
+        title: 'Cart',
+        isCart: true,
+        courses: cart.courses,
+        price: cart.price
+    });
+});
 
-module.exports = router
+
+module.exports = router;
