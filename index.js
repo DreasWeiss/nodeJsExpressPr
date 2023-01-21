b1bca5dac9ac3a9d81f7b56ade56cae2d0146de9
DreasWeiss <dreasweiss@gmail.com>
2023-01-21 18:55:22 +0300

    mongodb created and connected - added module mongoose - added module dotenv - in index.js file created new start function with connection to data base

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
+const mongoose = require('mongoose');
 const homeRoutes = require('./routes/home');
 const coursesRoutes = require('./routes/courses');
 const addRoutes = require('./routes/add');
@@ -17,18 +19,30 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
-app.use(express.static(path.join(__dirname,'public')));
-app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 //Routes
 app.use('/', homeRoutes);
 app.use('/courses', coursesRoutes);
 app.use('/add', addRoutes);
 app.use('/cart', cartRoutes);
 
+async function start() {
+    try {
+        const mongoDbUri = `mongodb+srv://${process.env.MONGODBUSER}:${process.env.MONGODBPSW}@cluster0.7t8l1ua.mongodb.net/?retryWrites=true&w=majority`;
+        await mongoose.connect(mongoDbUri);
 
-const port = process.env.PORT || 3000;
-const listener = app.listen(port, () => {
-    console.log(
-        `App is listening on port: ${port} \n http://localhost:${port}`
-        );
-});
+        const port = process.env.PORT || 3000;
+        const listener = app.listen(port, () => {
+            console.log(
+                `App is listening on port: ${port} \n http://localhost:${port}`
+            );
+        });
+
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+start();
