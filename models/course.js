f0e5c1e6988c43c4823933ed9c77171b51aa9ac3
DreasWeiss <dreasweiss@gmail.com>
2023-01-21 19:41:22 +0300

    created schema for model

--- models/course.js
+++ models/course.js
@@ -1,85 +1,15 @@
-const { v4: uuid } = require('uuid');
-const fs = require('fs');
-const path = require('path');
+const {Schema, model} = require('mongoose');
 
-class Course{
-    constructor(title, price, img) {
-        this.title = title;
-        this.price = price;
-        this.img = img;
-        this.id = uuid();
-    }
+const course = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    img: String
+});
 
-    toJSON(){
-        return {
-            title: this.title,
-            price: this.price,
-            img: this.img,
-            id: this.id
-        };
-    }
-
-    async save(){
-        const courses = await Course.getAll();
-        
-        courses.push(this.toJSON());
-
-        return new Promise ((resolve, reject) => {
-            fs.writeFile(
-                path.join(__dirname,'..','data','courses.json'),
-                JSON.stringify(courses),
-                (err) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve();
-                    }
-                }
-            );
-        });  
-    }
-
-    static getAll(){
-        return new Promise ((resolve, reject) => {
-            fs.readFile(
-                path.join(__dirname,'..','data','courses.json'),
-                'utf-8',
-                (err, content) => {
-                    if(err) {
-                        reject(err);
-                    } else {
-                        resolve(JSON.parse(content));
-                    }
-                }
-            );
-        });
-    }
-
-    static async getById(id){
-        const courses = await Course.getAll();
-        return courses.find(course => course.id === id);
-    }
-
-    static async update(course){
-        const courses = await Course.getAll();
-
-        const idx = courses.findIndex(c => c.id === course.id);
-        courses[idx] = course;
-
-        return new Promise ((resolve, reject) => {
-            fs.writeFile(
-                path.join(__dirname,'..','data','courses.json'),
-                JSON.stringify(courses),
-                (err) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve();
-                    }
-                }
-            );
-        }); 
-    }
-}
-
-module.exports = Course;
+module.exports = model('Course', course);
