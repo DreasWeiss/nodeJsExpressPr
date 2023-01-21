9661ca2b4650899f396e2f5d04b54e141600e75b
DreasWeiss <dreasweiss@gmail.com>
2023-01-21 20:38:28 +0300

    updated routes/courses.js file to work with mongodb

--- routes/courses.js
+++ routes/courses.js
@@ -3,7 +3,7 @@ const Course = require('../models/course');
 const router = Router();
 
 router.get('/', async (req, res) => {
-    const courses = await Course.getAll();
+    const courses = await Course.find();
     res.render('courses', {
         title: 'Courses',
         isCourses: true,
@@ -12,7 +12,7 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-    const course = await Course.getById(req.params.id);
+    const course = await Course.findById(req.params.id);
     res.render('course', {
         layout: 'empty',
         title: `Course: ${course.title}`,
@@ -25,7 +25,7 @@ router.get('/:id/edit', async (req, res) => {
         return res.redirect('/');
     }
 
-    const course = await Course.getById(req.params.id);
+    const course = await Course.findById(req.params.id);
 
     res.render('course-edit', {
         title: `Edit ${course.title}`,
@@ -34,7 +34,9 @@ router.get('/:id/edit', async (req, res) => {
 });
 
 router.post('/edit', async (req, res) => {
-    await Course.update(req.body);
+    const {id} = req.body;
+    delete req.body.id;
+    await Course.findByIdAndUpdate(id, req.body);
     res.redirect('/courses');
 });
 
