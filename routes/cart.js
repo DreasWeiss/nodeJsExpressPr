7e81bab967c15efb979f9466d0d847c36727aeb8
DreasWeiss <dreasweiss@gmail.com>
2023-01-22 22:17:43 +0300

    fix render cart
    /routes/cart.js
    - fix router.get
    - add functions:
        + mapCartItems(cart) to have arr with obj course + count
        + computePrice to have total price for order in cart

--- routes/cart.js
+++ routes/cart.js
@@ -2,6 +2,18 @@ const {Router} = require('express');
 const Course = require('../models/course');
 const router = Router();
 
+function mapCartItems(cart) {
+    return cart.items.map(c => ({
+        ...c.courseId._doc , count: c.count
+    }));
+}
+
+function computePrice(courses) {
+    return courses.reduce( (total, course) => {
+        return total += course.price * course.count
+    }, 0);
+}
+
 router.post('/add', async (req, res) => {
     const course = await Course.findById(req.body.id);
     await req.user.addToCart(course);
@@ -14,12 +26,16 @@ router.delete('/remove/:id', async (req, res) => {
 });
 
 router.get('/', async (req, res) => {
-    const cart = await Cart.fetch();
+    const user = await req.user
+        .populate('cart.items.courseId');
+
+    const courses = mapCartItems(user.cart);
+
     res.render('cart', {
         title: 'Cart',
         isCart: true,
-        courses: cart.courses,
-        price: cart.price
+        courses: courses,
+        price: computePrice(courses)
     });
 });
 
