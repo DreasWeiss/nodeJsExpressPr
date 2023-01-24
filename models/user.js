f5e73703e38f1de69bc373cb88913b0eeef4904c
DreasWeiss <dreasweiss@gmail.com>
2023-01-24 16:52:31 +0300

    Get orders data
    - add new method clearCart for User model
    to clear users cart after order
    
    /routes/orders.js
    - add logic for get/ for orders routes
    - fix post/ with try/catch

--- models/user.js
+++ models/user.js
@@ -60,4 +60,9 @@ userSchema.methods.removeFromCart = function (id) {
     return this.save();
 };
 
+userSchema.methods.clearCart = function () {
+    this.cart = { items: [] };
+    return this.save();
+};
+
 module.exports = model('User', userSchema); 
--- routes/orders.js
+++ routes/orders.js
@@ -3,14 +3,47 @@ const Order = require('../models/order');
 const router = Router();
 
 router.get('/', async (req, res) => {
-    res.render('orders', {
-        title: "Orders",
-        isOrders: true
-    });
+    try {
+        const orders = await Order.find({
+            'user.userId': req.user._id
+        }).populate('user.userId');
+        res.render('orders', {
+            title: "Orders",
+            isOrders: true,
+            orders: orders.map(o => {
+                return {
+                    ...o._doc,
+                    price: o.courses.reduce((total, course) => {
+                        return total += course.count * course.course.price;
+                    }, 0)
+                };
+            })
+        });
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 router.post('/', async (req, res) => {
-    res.redirect('/orders');
+    try {
+        const user = await req.user.populate('cart.items.courseId');
+        const courses = user.cart.items.map(i => ({
+            count: i.count,
+            course: { ...i.courseId._doc }
+        }));
+        const order = new Order({
+            user: {
+                name: req.user.name,
+                userId: req.user
+            },
+            courses
+        });
+        await order.save();
+        await req.user.clearCart();
+        res.redirect('/orders');
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 module.exports = router;
