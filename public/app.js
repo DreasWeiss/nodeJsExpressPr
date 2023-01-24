f034af02c17cd11442fc005c45f71e0acf31c9bd
DreasWeiss <dreasweiss@gmail.com>
2023-01-24 17:33:39 +0300

    Order page =>  output orders
    - write hbs file for orders page
    - add function toDate(date) in client scripts: /public/app.js
    now the output date looks more elegant

--- public/app.js
+++ public/app.js
@@ -3,10 +3,25 @@ const toCurrency = price => {
         currency: 'eur',
         style: 'currency'
     }).format(price);
-}
+};
+
+const toDate = date => {
+    return new Intl.DateTimeFormat('de-DE', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    }).format(new Date(date));
+};
 
 document.querySelectorAll('.price').forEach(node => {
     node.textContent = toCurrency(node.textContent);
+});
+
+document.querySelectorAll('.date').forEach(node => {
+    node.textContent = toDate(node.textContent);
 });
 
 const $cart = document.querySelector('#cart');
@@ -42,3 +57,4 @@ if ($cart) {
         }
     });
 }
+
